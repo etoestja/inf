a46d4bf80c8337077f3b014f14910696cc51e41a
Sergey Volodin <etoestja@yandex.ru>
2013-04-22 18:38:44 +0400

    11

---
+++ mipt/s2/11/StierlitzTest/main.asm
@@ -0,0 +1,19 @@
+;[ORG 0x7C00]
+
+global _start
+
+section .text
+_start:
+mov eax, 4
+mov ebx, 1
+mov ecx, theString
+mov edx, theLength
+int 0x80
+
+mov ebx, 0
+mov eax, 1
+int 0x80
+
+section .data
+theString: db "Stierlitz"
+theLength equ $ - theString
