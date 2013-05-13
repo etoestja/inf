63e5b212cca4c67e0facbd868a5f8d82a9ebfeb3
Sergey Volodin <etoestja@yandex.ru>
2013-05-13 19:45:44 +0400

    old test

--- mipt/s2/data/y/f.c
+++ mipt/s2/data/y/f.c
@@ -3,6 +3,7 @@ long double Ryad(int N)
     long double r = 0, t;
     int i;
     for(i = N; i >= 1; i--)
+//    for(i = 1; i <= N; i++)
     {
         t = (long double) i;
         r += ((long double) 1.) / (t * (t + 2));
