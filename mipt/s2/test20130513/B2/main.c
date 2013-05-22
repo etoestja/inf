167c318fc9ee1a457528d8d860fa898c16c60fdc
Sergey Volodin <etoestja@yandex.ru>
2013-05-22 14:33:16 +0400

    B2 (libleap2)

---
+++ mipt/s2/test20130513/B2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int leap(int year);
+
+void test(int year)
+{
+    if(leap(year))
+        printf("%d\n", year);
+}
+
+int main()
+{
+    int i = 0;
+
+    test(0);
+    for(i = 1; i != 0; i++)
+        test(i);
+
+
+    printf("%d\n", leap(2057));
+    return(0);
+}
