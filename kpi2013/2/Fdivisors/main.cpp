828342bf4bd2d4fae08f5357c0c1653208410452
Sergey Volodin <etoestja@yandex.ru>
2013-07-07 22:47:00 +0400

    Fdivisord

--- kpi2013/2/Fdivisors/main.cpp
+++ kpi2013/2/Fdivisors/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <iostream>
 #include <algorithm>
 
@@ -12,6 +13,69 @@ using namespace std;
 long long arr[NMAX];
 long long N;
 long long T;
+
+#define RC 150
+
+long long longPow(long long a, long long b, long long m)
+{
+    long long res = 1;
+    long long i;
+    for(i = 0; i < b; i++)
+    {
+        res %= m;
+        res *= a;
+        res %= m;
+    }
+    return(res);
+}
+
+bool primeTest(long long a, long long s, long long t, long long m)
+{
+    long long x = longPow(a, t, m);
+    //cerr << "x=" << x << endl;
+    if(x == 1)
+        return(true);
+    for(long long i = 1; i <= s - 1; i++)
+    {
+        if(x == m - 1)
+            return(true);
+        x = (x * x) % m;
+    }
+    //cerr << "x=" << x << endl;
+    return(x == m - 1);
+}
+
+bool isPrime(long long m)
+{
+    if(m % 2 == 0) return(false);
+    if(m <= RC)
+    {
+        for(int i = 2; i < m; i++)
+        {
+            if(m % i == 0)
+                return(false);
+        }
+        return(true);
+    }
+
+    long long s = 0;
+    long long t = m - 1;
+    while(t % 2 == 0)
+    {
+        t /= 2;
+        s++;
+    }
+    //cerr << m << "-1=" << "2**" << s << "*" << t << endl;
+
+    for(long long a = 3; a < RC; a++)
+        if(!primeTest(a, s, t, m))
+        {
+            cerr << a << " " << s << " " << t << " " << m << endl;
+            return(false);
+        }
+
+    return(true);
+}
 
 inline bool llcmp(long long a, long long b)
 {
@@ -43,6 +107,25 @@ long long countDiv(long long a)
     return(res);
 }
 
+long long llSqrt(long long a)
+{
+    long long l = 1;
+    long long r = sqrt(a) + 100;
+    long long m;
+    while(r - l > 1)
+    {
+        m = (r + l) / 2;
+        if(m * m > a)
+            r = m;
+        else l = m;
+    }
+    if(l * l == a)
+        return(l);
+    else if(r * r == a)
+        return(r);
+    return(-1);
+}
+
 long long divisorsN(long long a)
 {
     long long res = 1;
@@ -63,46 +146,11 @@ long long divisorsN(long long a)
 
     if(a > 1)
     {
-        long long count = countDiv(a);
-#ifdef DEBUG
-        cerr << "a=" << a << endl;
-        cerr << "count=" << count << endl;
-#endif
-        if(count == 4)
-        {
-#ifdef DEBUG
-            cerr << "count = 4" << endl;
-#endif
-            res *= 4;
-        }
-        else if(count == 3 || count == 2)
-        {
-#ifdef DEBUG
-            cerr << "count in {2, 3}" << endl;
-#endif
-            for(long long i = 0; i < N; i++)
-                if((a % arr[i]) == 0 && arr[i] != 1)
-                {
-                    if(a == arr[i])
-                        res *= 2;
-                    else
-                    {
-                        if(a / arr[i] == arr[i])
-                            res *= 3;
-                        else
-                            res *= 4;
-                    }
-                    break;
-                }
-        }
-        else
-        {
-#ifdef DEBUG
-            cerr << "count not in {2, 3, 4}" << endl;
-#endif
-            cout << "no" << endl;
-            exit(0);
-        }
+        if(isPrime(a)) //p
+            res *= 2;
+        else if(llSqrt(a) != -1) // p^2
+            res *= 3;
+        else res *= 4; //pq
     }
     return(res);
 }
