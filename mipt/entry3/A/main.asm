1a929dcb92e976ef02135f32a73c11fcb3dc9bd9
etoestja <etoestja@yandex.ru>
2013-09-01 18:48:30 +0400

    entry3

---
+++ mipt/entry3/A/main.asm
@@ -0,0 +1,17 @@
+global _start
+
+section .text
+_start:
+mov eax, 4
+mov ebx, 1
+mov ecx, theString
+mov edx, theLength
+int 0x80
+
+mov ebx, 0
+mov eax, 1
+int 0x80
+
+section .data
+theString: db "MIPT", 0xA
+theLength equ $ - theString
---
+++ mipt/entry3/A/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("MIPT\n");
+    return(0);
+}
