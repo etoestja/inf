467127226753eeb57faa8ef327efc43538d4cf6b
Sergey Volodin <etoestja@yandex.ru>
2013-09-14 16:54:36 +0400

    task1: useless update

--- mipt/s3/inf/task1/useless/src/main.c
+++ mipt/s3/inf/task1/useless/src/main.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define FILENAME_MAXLEN 1000
+#define NAMEARGS_MAXLEN 1000
+#define MAXPROGS 1000
+
 typedef struct
 {
     int delay;
-    char* nameArgs;
+    char nameArgs[NAMEARGS_MAXLEN];
 } program;
+
+program arr[MAXPROGS];
 
 int programCmp(const void *a, const void *b)
 {
     return(((program*) a)->delay > ((program*) b)->delay);
 }
 
-#define FILENAME_MAXLEN 1000
-#define MAXPROGS 1000
 char filename[FILENAME_MAXLEN];
 
 void parseFile()
 {
     FILE *fd = fopen(filename, "r");
+
+    int tTime;
+    char [NAMEARGS_MAXLEN]
+    while(fscanf(fd, "%d", &tTime))
+    {
+        printf("got time=%d and prog=%s\n", tTime, );
+    }
     
     fclose(fd);
 }
