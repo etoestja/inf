3650ee71fec343293b91a7b0a8c8fd36d588064d
Sergey Volodin <etoestja@yandex.ru>
2013-09-14 14:50:30 +0400

    waitpid, fork != -1

--- mipt/s3/forktest2/main.c
+++ mipt/s3/forktest2/main.c
@@ -6,7 +6,19 @@ int main()
     printf("started\tPID=%d\tPPID=%d\n", getpid(), getppid());
 
     int res = fork();
+
+    if(res == -1)
+    {
+        printf("can't fork. exiting...\n");
+        return(1);
+    }
+
+#ifndef NOWAITPID
+    if(res) waitpid(res, NULL, 0);
+#endif
+
     printf("%s\tPID=%d\tPPID=%d\n", res ? "parent" : "child",
         getpid(), getppid());
+
     return(0);
 }
