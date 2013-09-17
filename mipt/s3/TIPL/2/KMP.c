265b193484d39871658eb25fa8f82fcc2446e0d0
Sergey Volodin <etoestja@yandex.ru>
2013-09-17 21:41:53 +0400

    TIPL task7: typo fixes

--- mipt/s3/TIPL/2/KMP.c
+++ mipt/s3/TIPL/2/KMP.c
@@ -13,6 +13,16 @@ void makeS()
     sprintf(S, "*%s#%s", P, T);
 }
 
+// prints S[1]...S[p]
+void printSPrefix(int p)
+{
+    int i;
+    printf("<");
+    for(i = 1; i <= p; i++)
+        printf("%c", S[i]);
+    printf(">\t");
+}
+
 // calculates pi(string[1]...string[len]) and stores to prefix[1]...prefix[len]
 void fillPrefix(char* string)
 {
@@ -22,46 +32,54 @@ void fillPrefix(char* string)
 
     for(i = 0; i <= l - 1; i++)
     {
-        printf("fillPrefix at i=%d, looking at symbol %c\n", i, string[i + 1]);
+        printf("Looking at [%d], %c:", i + 1, string[i + 1]);
         t = prefix[i];
+        printf("\tlastP=");
+        printSPrefix(t);
 
         while(string[i + 1] != string[t + 1])
         {
             t = prefix[t];
+            printf("lastP=");
+            printSPrefix(t);
             if(t == 0)
             {
-                printf("  zero prefix[%d, %c]\n", t, string[t]);
+                printf("break\t");
                 prefix[i + 1] = 0;
                 break;
             }
-            else
-                printf("  going deeper[%d, %c]\n", t, string[t]);
         }
         if(string[i + 1] == string[t + 1] && i != t)
         {
-                printf("  got prefix[%d, %c] . '%c'\n", t, string[t], string[i + 1]);
-                prefix[i + 1] = t + 1;
+            prefix[i + 1] = t + 1;
+            printf("Found P=");
+            printSPrefix(prefix[i + 1]);
         }
+        else
+        {
+            printf("Found P=<>");
+        }
+        printf("\n");
     }
 }
 
 void printPrefix(char* string)
 {
     int i;
-    printf("n\t");
+    printf("n ");
     for(i = 0; i < strlen(string); i++)
     {
         if(i < 10)
-            printf("%d\t", i);
+            printf("%d ", i);
         else
-            printf("%c\t", 'A' + i - 10);
+            printf("%c ", 'A' + i - 10);
     }
-    printf("\nc\t");
+    printf("\nc ");
     for(i = 0; i < strlen(string); i++)
-        printf("%c\t", string[i]);
-    printf("\np\t");
+        printf("%c ", string[i]);
+    printf("\np ");
     for(i = 0; i < strlen(string); i++)
-        printf("%d\t", prefix[i]);
+        printf("%d ", prefix[i]);
     printf("\n");
 }
 
@@ -70,7 +88,7 @@ void findPInT()
     int i;
     for(i = 1; i <= strlen(S); i++)
         if(prefix[i] == strlen(P))
-            printf("findPInT: found P in T starting from position %d\n", i - strlen(P) + 1);
+            printf("Found, starting from %d\n", i - strlen(P) + 1);
 }
 
 int main()
@@ -79,8 +97,11 @@ int main()
     scanf("%s", &T);
 
     makeS();
+    printf("Filling prefix[]\n");
     fillPrefix(S);
+    printf("\nPrefix function:\n");
     printPrefix(S);
+    printf("\nResult:\n");
     findPInT();
 
     return(0);
