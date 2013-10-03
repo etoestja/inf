71174841948bebfc725c31e074baf6520d55f6c0
Sergey Volodin <etoestja@yandex.ru>
2013-10-03 21:22:13 +0400

    Theormech 2.36w fxd

--- mipt/s3/Theormech/2.36w.py
+++ mipt/s3/Theormech/2.36w.py
@@ -5,8 +5,8 @@ def mine(phi0,w,t,w0,R):
  phitt=-phi0*sin(w*t)*w**2
  theta=-w0*t**2/(2.*R)
  s=-((phit*R-w0*t)**2)/R
- x=s*cos(phi+theta)-R*phitt*sin(phi+theta)
- y=w0+s*sin(phi+theta)+R*phitt*cos(phi+theta)
+ x=s*cos(phi+theta)-R*phitt*sin(phi+theta)+w0*sin(phi+theta)
+ y=w0+s*sin(phi+theta)+R*phitt*cos(phi+theta)-w0*cos(phi+theta)
  return((x**2+y**2)**0.5)
 
 def theirs(phi0,w,t,w0,R):
@@ -16,11 +16,11 @@ def theirs(phi0,w,t,w0,R):
  y=2*w0*sin(phi1/2.)**2+R*w**2*phi0*sin(w*t)
  return((x**2+y**2)**0.5)
 
-phi0=1.
-w=1.123
-t=1
-w0=5.
-R=12.
+phi0=12143.
+w=1212.123
+t=2132.1
+w0=52134.234
+R=121234.2143
 
 def test():
  print theirs(phi0,w,t,w0,R)
