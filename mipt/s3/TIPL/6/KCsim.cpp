3477feb195779b7bc45bda695a843e80a8b976b8
Sergey Volodin <etoestja@yandex.ru>
2013-10-15 22:34:39 +0400

    TIPL 6 1

--- mipt/s3/TIPL/6/KCsim.cpp
+++ mipt/s3/TIPL/6/KCsim.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+//#undef DEBUG
+#define DEBUG
+//#define OUTPUT_TEX
+//#define OUTPUT_NORMAL
+//#define OUTPUT_TEXRULES
+
+
 typedef pair< string, string > rule;
 vector<rule> rules;
 
@@ -13,6 +20,18 @@ string printRule(int i, rule a)
     stringstream ss;
     ss << "[" << a.first << "] =(" << i << ")=> " << "[" << a.second << "]";
     return(ss.str());
+}
+
+void printRulesTex()
+{
+    rule a;
+    for(int i = 0; i < rules.size(); i++)
+    {
+        a = rules[i];
+        if(a.second.length() == 0)
+            a.second = "\\varepsilon";
+        cout << "\\item $" << a.first << " \\longrightarrow " << a.second << "$" << endl;
+    }
 }
 
 void readRules()
@@ -40,12 +59,22 @@ int applyRule(int p)
     int pos2 = current.rfind(r.first);
     if(pos1 != pos2)
     {
+#ifdef DEBUG
         cout << " Found occurence at " << pos1 << " and " << pos2 << "!" << endl;
+#endif
         return(-1);
     }
     int pos = pos1;
+#ifdef OUTPUT_PLAIN
+    cout << current.substr(0, pos) << "[" << r.first << "]" << current.substr(pos + r.first.length()) << " =(" << p + 1 << ")=> ";
+#endif
+#ifdef OUTPUT_TEX
+    cout << current.substr(0, pos) << "\\underline{" << r.first << "}" << current.substr(pos + r.first.length()) << " \\overset{(" << p + 1 << ")}{\\Longrightarrow}";
+#endif
     string res = current.replace(pos, r.first.length(), r.second);
+#ifdef DEBUG
     cout << " RTO [" << res << "]" << endl;
+#endif
     current = res;
     return(0);
 }
@@ -63,22 +92,28 @@ int applyRules()
                 p = i;
             else
             {
+#ifdef DEBUG
                 cout << "ERROR: can apply to [" << current << "] both " << p << " and " << i << endl;
                 cout << " " << printRule(p, rules[p]) << endl;
                 cout << " " << printRule(i, rules[i]) << endl;
+#endif
                 return(-2);
             }
         }
     }
     if(p != -1)
     {
+#ifdef DEBUG
         cout << "Applying rule " << printRule(p, rules[p]) << endl;
+#endif
         if(applyRule(p) != 0) return(-1);
         return(0);
     }
     else
     {
+#ifdef DEBUG
         cout << "Can't apply to [" << current << "] anything..." << endl;
+#endif
         return(1);
     }
 }
@@ -98,5 +133,14 @@ void applyLoop()
 int main()
 {
     init();
+#ifdef OUTPUT_TEXRULES
+    printRulesTex();
+#endif
     applyLoop();
+#ifdef OUTPUT_TEX
+    cout << current << endl;
+#endif
+#ifdef OUTPUT_PLAIN
+    cout << current << endl;
+#endif
 }
