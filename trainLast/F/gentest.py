195fb60ecd1daeb9cf9fc865e0cc5e800dcf0f79
Sergey Volodin <etoestja@yandex.ru>
2013-10-19 19:41:47 +0400

    inf lastTrain F ??

---
+++ trainLast/F/gentest.py
@@ -0,0 +1,10 @@
+N=20
+i=0
+print str(N*5)
+while i<N:
+ print "cat"
+ print "bbb"
+ print "ololo"
+ print "lala"
+ print "icpcicpc"
+ i+=1
---
+++ trainLast/F/gentest1.py
@@ -0,0 +1,6 @@
+N=100
+i=0
+print str(N)
+while i<N:
+ print "abcdefghijab"
+ i+=1
