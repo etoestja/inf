3234074300eb507127a3eaddd8debb1dde4114a7
etoestja <etoestja@yandex.ru>
2013-10-19 14:49:41 +0400

    inf 7 lseek

---
+++ mipt/s3/inf/7/lseek.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	if(argc < 2)
+		return(-1);
+	int fd = open(argv[1], O_RDONLY);
+	int pos = lseek(fd, 0, SEEK_END);
+	printf("size of %s = %d\n", argv[1], pos);
+	return(0);
+}
