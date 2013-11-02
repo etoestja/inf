1eec4b44a70b2ebaaf9ad550facd2248692bdb7a
Sergey Volodin <etoestja@yandex.ru>
2013-11-02 20:36:26 +0400

    inf 9 lsl2

--- mipt/s3/inf/9/lsl/main.c
+++ mipt/s3/inf/9/lsl/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+int filter(const struct dirent *item)
+{
+    return(1);
+}
 
 int main(int argc, char* argv[])
 {
@@ -11,35 +19,35 @@ int main(int argc, char* argv[])
         return(-1);
     }
 
-    DIR* dir = opendir(argv[1]);
-
-    if(dir == NULL)
-    {
-        printf("opendir failed [%s]\n", argv[1]);
-        return(-1);
-    }
-
-    int count = 0;
-
-    struct dirent* directory;
-    while(readdir(dir) != NULL)
-        count++;
+    struct dirent **contents;
+    int count = scandir(argv[1], &contents, filter, alphasort);
 
     printf("total %d\n", count);
 
-    rewinddir(dir);
-
-    while((directory = readdir(dir)) != NULL)
+    if(count < 0)
     {
-        printf("%s ", directory->d_name);
-        printf("\n");
-    }
-    
-    if(closedir(dir) < 0)
-    {
-        printf("closedir failed [%s]\n", argv[1]);
+        printf("scandir failed [%s]\n", argv[1]);
         return(-1);
     }
+    if(count == 0)
+        return(0);
 
+    int i;
+    struct stat info;
+    char* path = NULL;
+    for(i = 0; i < count; i++)
+    {
+        path = malloc(sizeof(char) * (strlen(argv[1]) + 1 + strlen(contents[i]->d_name) + 1));
+        strcpy(path, argv[1]);
+        strcat(path, "/");
+        strcat(path, contents[i]->d_name);
+        printf("%s, %s\n", contents[i]->d_name, path);
+        free(path);
+        free(contents[i]);
+        path = NULL;
+    }
+
+    free(contents);
+    
     return(0);
 }
