95a2e76dcf84b94e7be3ac350b2e7105a1936189
Sergey Volodin <etoestja@yandex.ru>
2013-11-09 16:26:57 +0400

    inf A

---
+++ mipt/s3/inf/A/client-test.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <fcntl.h>
+#include <netdb.h>
+
+/* Номер порта, который обслуживается сервером */
+#define PORTNUM 1500
+
+main(argc, argv)
+int argc;
+char *argv[];
+{
+  int s;
+  int pid;
+  int i, j;
+  struct sockaddr_in serv_addr;
+  struct hostent *hp;
+  char buf[80] = "Hello, World!";
+
+  /* В качестве аргумента клиенту передается доменное имя хоста,
+     но котором запущен сервер. Произведем трансляцию доменного
+     имени в адрес */
+  if((hp = gethostbyname(argv[1])) == 0){
+    perror("Ошибка вызова gethostbyname()!\n"); exit(-1);
+  }
+  bzero(&serv_addr, sizeof(serv_addr));
+  bcopy(hp->h_addr, &serv_addr.sin_addr, hp->h_length);
+  serv_addr.sin_family = hp->h_addrtype;
+  serv_addr.sin_port = htons(PORTNUM);
+
+  /* Создадим сокет */
+  if((s = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    perror("Ошибка вызова socket()!\n"); exit(-1);
+  }
+
+  fprintf(stderr, "Адрес сервера: %s\n", inet_ntoa(serv_addr.sin_addr));
+
+  /* Создадим виртуальный канал */
+  if(connect(s, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
+    perror("Ошибка вызова connect()!\n"); exit(-1);
+  }
+
+  /* Отправим серверу сообщение и получим его обратно */
+  send(s, buf, sizeof(buf), 0);
+
+  if(recv(s, buf, sizeof(buf), 0) < 0){
+    perror("Ошибка вызова recv()!\n"); exit(-1);
+  }
+
+  /* Выведем полученное сообщение на экран */
+  printf("Получено от сервера: %s\n", buf);
+  close(s);
+  printf("Клиент завершил работу \n\n");
+}
