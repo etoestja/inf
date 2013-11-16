9b691d837e0b75dee3036f2f9f549a308b8d1284
Sergey Volodin <etoestja@yandex.ru>
2013-11-16 14:46:28 +0400

    inf A chat4 client ~

--- mipt/s3/inf/A/chat4/server.c
+++ mipt/s3/inf/A/chat4/server.c
@@ -1,5 +1,29 @@
 #include "common.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <errno.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+    int nport = htons((u_short) SERVER_PORT);
+    int serverSocket;
+    if((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Can't get socket\n");
+        return(-1);
+    }
+
+    struct sockaddr_in servAddr, clntAddr;
+
+    bzero(&servAddr, sizeof(servAddr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = nport;
+
+
 }
