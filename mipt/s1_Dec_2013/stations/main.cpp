e21d99b9901ec19dd2f25ee85f5d12a3f7e4d3b8
Sergey Volodin <etoestja@yandex.ru>
2013-12-08 19:30:57 +0400

    s1_Dec_2013 stations proper

--- mipt/s1_Dec_2013/stations/main.cpp
+++ mipt/s1_Dec_2013/stations/main.cpp
@@ -13,29 +13,27 @@ using std::set;
 
 unsigned int M,N;
 
-#define NMAX 2013
-
-int arr[NMAX][NMAX];
-
 void nofprintf(FILE* a, ...)
 {
 }
 
 #define fprintf nofprintf
 
-int start;
-
 map< unsigned int,vector<unsigned int> > g;
 vector<unsigned int> used;
-vector<unsigned int> okArr;
+
+vector<unsigned int> isset;
+vector<unsigned int> found;
 
 void dfs1(unsigned int v) {
-    arr[start][v] = 1;
 	used[v]=1;
+    found[v] = 1;
+    isset[v] = 0;
 	if(g.find(v)!=g.end()) {
 		vector<unsigned int>::iterator it;
 		for(it=g[v].begin();it!=g[v].end();it++) {
-			if(!used[*it]) dfs1(*it);
+			if(!used[*it])
+                dfs1(*it);
 		}
 	}
 }
@@ -43,11 +41,6 @@ void dfs1(unsigned int v) {
 int main() {
 	unsigned int i,si,t1,t2;
 	cin >> N >> M;
-
-    for(t1 = 0; t1 < NMAX; t1++)
-        for(t2 = 0; t2 < NMAX; t2++)
-            arr[t1][t2] = 0;
-	
 	vector<unsigned int> tvect;
 	
 	for(i=0;i<M;i++) {
@@ -61,59 +54,22 @@ int main() {
 		}
 	}
 	
-
-	for(i=0;i<N;i++) {
-        start = i;
-	    used.assign(N,0);
-        dfs1(i);
-	}
-
-    for(t1 = 0; t1 < N; t1++)
-    {
-        for(t2 = 0; t2 < N; t2++)
-            fprintf(stderr, "%d ", arr[t1][t2]);
-        fprintf(stderr, "\n");
-    }
-
-	okArr.assign(N,0);
-    int OKCount = 0;
-
-    int probeOK;
-    int maxProbeOK;
-    int maxI;
-
+    isset.assign(N + 1, 0);
+    found.assign(N + 1, 0);
     int res = 0;
 
-    while(OKCount < N)
-    {
-        maxProbeOK = 0;
-        for(i = 0; i < N; i++)
+	for(i=0;i<N;i++) {
+        used.assign(N + 1, 0);
+        if(!found[i])
         {
-            probeOK = 0;
-            for(t1 = 0; t1 < N; t1++)
-            {
-                if(arr[t1][i] && !okArr[t1])
-                    probeOK++;
-            }
-            fprintf(stderr, "v=%d, probeOK=%d\n", i, probeOK);
-
-            if(probeOK > maxProbeOK)
-            {
-                maxProbeOK = probeOK;
-                maxI = i;
-            }
+            dfs1(i);
+            isset[i] = 1;
+            fprintf(stderr, "setting to %d\n", i);
         }
+	}
 
-        fprintf(stderr, "maxI = %d, maxProbeOK = %d\n", maxI, maxProbeOK);
-
-        for(t1 = 0; t1 < N; t1++)
-            if(arr[t1][maxI] && !okArr[t1])
-                okArr[t1] = 1;
-
-        fprintf(stderr, "to %d\n", maxI);
-        OKCount += maxProbeOK;
-        res++;
-    }
+    for(int i = 0; i < N; i++)
+        if(isset[i]) res++;
 
     cout << res << endl;
 
