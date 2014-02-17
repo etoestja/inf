9d26b8353b0811542990b21175ea35e5aa564c45
Sergey Volodin <etoestja@yandex.ru>
2014-02-17 17:48:00 +0300

    DB upd

--- mipt/s4/DB/1/task2.sql
+++ mipt/s4/DB/1/task2.sql
@@ -1,8 +1,9 @@
 /*
 e1 < e2
 */
-select distinct e2.* from employee e1,employee as e2
+select distinct e2.first_name + ' ' + e2.middle_initial + ' ' + e2.last_name as name from employee e1, employee as e2
 where e1.manager_id=e2.employee_id
 and e1.job_id in (select job_id from job where [function]='ANALYST')
 and e2.job_id in (select job_id from job where [function]='MANAGER')
-and e1.hire_date < dateadd(year, 5, e2.hire_date)
+and e1.hire_date <= dateadd(year, 5, e2.hire_date)
+and e1.hire_date >= e2.hire_date
--- mipt/s4/DB/1/task4.sql
+++ mipt/s4/DB/1/task4.sql
@@ -1,6 +1,7 @@
-select top 5 sum(sales_order.total),product.product_id from sales_order, product, item
+select top 5 sum(sales_order.total), product.product_id, product.description
+from sales_order, product, item
 where sales_order.order_id = item.order_id
 and item.product_id = product.product_id
 and year(sales_order.ship_date)=1989
-group by product.product_id
+group by product.product_id, product.description
 order by sum(sales_order.total) desc
