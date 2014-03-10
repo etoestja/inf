9a26ed2aaf1d68b052764e55a9fc206c689e124e
Sergey Volodin <etoestja@yandex.ru>
2014-03-10 12:10:03 +0300

    DB 3 insert initial

---
+++ mipt/s4/DB/3/insert.sql
@@ -0,0 +1,23 @@
+﻿use theater;
+
+insert into gender (description) values (N'М');
+insert into gender (description) values (N'Ж');
+
+/* полный список этой хуиты в википедии */
+insert into line (description) values (N'герой-любовник');
+insert into line (description) values (N'злодей');
+insert into line (description) values (N'герой');
+insert into line (description) values (N'героиня'); /* вот над этим надо подумать */
+insert into line (description) values (N'молодой любовник');
+insert into line (description) values (N'король');
+insert into line (description) values (N'тиран');
+insert into line (description) values (N'отец');
+insert into line (description) values (N'наперсник');
+insert into line (description) values (N'наперсница');
+insert into line (description) values (N'лакей');
+insert into line (description) values (N'наперсница');
+insert into line (description) values (N'субретка');
+insert into line (description) values (N'инженю');
+insert into line (description) values (N'резонер');
+insert into line (description) values (N'петиметр');
+insert into line (description) values (N'простак');
