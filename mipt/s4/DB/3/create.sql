33c7999ebf129a38d91e8a2a179d2f52e86381dd
Sergey Volodin <etoestja@yandex.ru>
2014-03-11 10:01:47 +0300

    DB 3

--- mipt/s4/DB/3/create.sql
+++ mipt/s4/DB/3/create.sql
@@ -5,38 +5,39 @@ go
 
 create table gender
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 description nvarchar(50) not null
 );
 go
 
 create table line
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 description nvarchar(50) not null
 );
 go
 
 create table roleType
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 description nvarchar(50) not null
 )
 go
 
 create table author
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(50) not null,
 birthDate datetime,
-deathDate datetime
+deathDate datetime,
+constraint bdate check (birthDate <= deathDate)
 )
 
 go
 
 create table play
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(50) not null,
 description nvarchar(500) not null
 )
@@ -45,7 +46,7 @@ go
 
 create table actorTitle
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 description nvarchar(50) not null
 )
 
@@ -53,39 +54,39 @@ go
 
 create table actor
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(50) not null,
 birthDate datetime,
-title int foreign key references actorTitle(id),
-gender int foreign key references gender(id)
+title int foreign key references actorTitle(id) on delete no action,
+gender int foreign key references gender(id) on delete no action
 )
 
 go
 
 create table actor_line
 (
-id int primary key identity(1,1),
-actor_id int foreign key references actor(id),
-line_id int foreign key references line(id)
+id int primary key identity(5000,1),
+actor_id int foreign key references actor(id) on delete cascade,
+line_id int foreign key references line(id) on delete cascade
 )
 
 go
 
 create table [role]
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(50) not null,
 description nvarchar(500),
-gender int foreign key references gender(id),
-line int foreign key references line(id),
-[type] int foreign key references roleType(id)
+gender int foreign key references gender(id) on delete no action,
+line int foreign key references line(id) on delete no action,
+[type] int foreign key references roleType(id) on delete no action
 )
 
 go
 
 create table director
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(50) not null
 )
 
@@ -93,13 +94,13 @@ go
 
 create table seatType
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 description nvarchar(500) not null
 )
 
 create table room
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [name] nvarchar(500)
 )
 
@@ -107,75 +108,79 @@ go
 
 create table seat
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 seat int not null,
 row int not null,
-room int foreign key references room(id),
-[type] int foreign key references seatType(id)
+room int foreign key references room(id) on delete no action,
+[type] int foreign key references seatType(id) on delete no action,
+constraint seatzero check (seat >= 0),
+constraint rowzero check (row >= 0)
 )
 
 go
 
 create table staging
 (
-id int primary key identity(1,1),
-director int foreign key references director(id)
+id int primary key identity(5000,1),
+director int foreign key references director(id) on delete no action
 )
 
 go
 
 create table role_staging
 (
-id int primary key identity(1,1),
-[role] int foreign key references [role](id),
-staging int foreign key references staging(id)
+id int primary key identity(5000,1),
+[role] int foreign key references [role](id) on delete cascade,
+staging int foreign key references staging(id) on delete cascade
 )
 
 go
 
 create table [cast]
 (
-id int primary key identity(1,1),
-number int not null
+id int primary key identity(5000,1),
+number int not null,
+constraint numberzero check (number >= 0)
 )
 
 go
 
 create table actor_rolestaging
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 actor int foreign key references actor(id),
-role_staging int foreign key references role_staging(id),
+role_staging int foreign key references role_staging(id) on delete cascade,
 beginDate datetime not null,
 endDate datetime not null,
-[cast] int foreign key references [cast](id)
+[cast] int foreign key references [cast](id) on delete set null,
+constraint dates check (beginDate <= endDate)
 )
 
 go
 
 create table play_author
 (
-id int primary key identity(1,1),
-play int foreign key references play(id),
-author int foreign key references author(id)
+id int primary key identity(5000,1),
+play int foreign key references play(id) on delete cascade,
+author int foreign key references author(id) on delete cascade
 )
 
 go
 
 create table role_play
 (
-id int primary key identity(1,1),
-[role] int foreign key references [role](id),
-play int foreign key references play(id)
+id int primary key identity(5000,1),
+[role] int foreign key references [role](id) on delete cascade,
+play int foreign key references play(id) on delete cascade
 )
 
 go
 
 create table play_staging
 (
-id int primary key identity(1,1),
-play int foreign key references play(id),
-staging int foreign key references staging(id)
+id int primary key identity(5000,1),
+play int foreign key references play(id) on delete cascade,
+staging int foreign key references staging(id) on delete cascade
 )
 
 go
@@ -183,46 +188,47 @@ go
 /* a replaces b */
 create table castReplace
 (
-id int primary key identity(1,1),
-a int foreign key references [cast](id),
-b int foreign key references [cast](id)
+id int primary key identity(5000,1),
+a int foreign key references [cast](id) on delete cascade,
+b int foreign key references [cast](id),
+constraint neq check (not (a = b))
 )
 
 go
 
 create table cast_staging
 (
-id int primary key identity(1,1),
-[cast] int foreign key references [cast](id),
-staging int foreign key references staging(id),
+id int primary key identity(5000,1),
+[cast] int foreign key references [cast](id) on delete cascade,
+staging int foreign key references staging(id) on delete cascade,
 )
 
 go
 
 create table performance
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 [date] datetime not null,
-staging int foreign key references staging(id),
-cast_staging int foreign key references cast_staging(id),
+staging int foreign key references staging(id) on delete no action,
+cast_staging int foreign key references cast_staging(id) on delete no action,
 )
 
 go
 
 create table actorrolestaging_performance
 (
-id int primary key identity(1,1),
-actorrolestaging int foreign key references actor_rolestaging(id),
-performance int foreign key references performance(id),
+id int primary key identity(5000,1),
+actorrolestaging int foreign key references actor_rolestaging(id) on delete cascade,
+performance int foreign key references performance(id) on delete cascade,
 )
 
 go
 
 create table seat_performance
 (
-id int primary key identity(1,1),
+id int primary key identity(5000,1),
 seat int foreign key references seat(id),
-performance int foreign key references performance(id)
+performance int foreign key references performance(id) on delete cascade
 )
 
 go
--- mipt/s4/DB/3/insert.sql
+++ mipt/s4/DB/3/insert.sql
@@ -1,23 +1,82 @@
 ﻿use theater;
 
-insert into gender (description) values (N'М');
-insert into gender (description) values (N'Ж');
+go
 
-/* полный список этой хуиты в википедии */
-insert into line (description) values (N'герой-любовник');
-insert into line (description) values (N'злодей');
-insert into line (description) values (N'герой');
-insert into line (description) values (N'героиня'); /* вот над этим надо подумать */
-insert into line (description) values (N'молодой любовник');
-insert into line (description) values (N'король');
-insert into line (description) values (N'тиран');
-insert into line (description) values (N'отец');
-insert into line (description) values (N'наперсник');
-insert into line (description) values (N'наперсница');
-insert into line (description) values (N'лакей');
-insert into line (description) values (N'наперсница');
-insert into line (description) values (N'субретка');
-insert into line (description) values (N'инженю');
-insert into line (description) values (N'резонер');
-insert into line (description) values (N'петиметр');
-insert into line (description) values (N'простак');
+SET IDENTITY_INSERT gender ON
+
+insert into gender (id, description) values (1, N'М');
+insert into gender (id, description) values (2, N'Ж');
+
+SET IDENTITY_INSERT gender OFF
+
+go
+
+SET IDENTITY_INSERT line ON
+
+insert into line (id, description) values (1,  N'герой-любовник');
+insert into line (id, description) values (2,  N'злодей');
+insert into line (id, description) values (3,  N'герой');
+insert into line (id, description) values (4,  N'героиня'); -- ?
+insert into line (id, description) values (5,  N'молодой любовник');
+insert into line (id, description) values (6,  N'король');
+insert into line (id, description) values (7,  N'тиран');
+insert into line (id, description) values (8,  N'отец');
+insert into line (id, description) values (9,  N'наперсник');
+insert into line (id, description) values (10, N'наперсница');
+insert into line (id, description) values (11, N'лакей');
+insert into line (id, description) values (12, N'наперсница');
+insert into line (id, description) values (13, N'субретка');
+insert into line (id, description) values (14, N'инженю');
+insert into line (id, description) values (15, N'резонер');
+insert into line (id, description) values (16, N'петиметр');
+insert into line (id, description) values (17, N'простак');
+
+SET IDENTITY_INSERT line OFF
+
+go
+
+SET IDENTITY_INSERT roleType ON
+
+insert into roleType (id, description) values (1,  N'главная');
+insert into roleType (id, description) values (2,  N'вторая');
+insert into roleType (id, description) values (3,  N'эпизод');
+insert into roleType (id, description) values (4,  N'статист');
+
+SET IDENTITY_INSERT roleType OFF
+
+go
+
+SET IDENTITY_INSERT [role] ON
+
+insert into [role](id, [name], gender, line, [type]) values (1,  N'техничка-шпионка', 2, 17, 4);
+insert into [role](id, [name], gender, line, [type]) values (2,  N'Гамлет', 1, 3, 1);
+
+SET IDENTITY_INSERT [role] OFF
+
+go
+
+SET IDENTITY_INSERT author ON
+
+insert into author(id, [name], birthDate, deathDate) values (1, N'Шекспир', 2013-01-01, 2014-01-01)
+
+SET IDENTITY_INSERT author OFF
+
+go
+
+SET IDENTITY_INSERT play ON
+
+insert into play(id, [name], description) values (1,  N'Плохая пьеса', N'дескрипшен')
+
+SET IDENTITY_INSERT play OFF
+
+go
+
+insert into play_author(play, author) values (1, 1)
+
+go
+
+insert into role_play(role, play) values (1, 1)
+
+go
+
+use master
