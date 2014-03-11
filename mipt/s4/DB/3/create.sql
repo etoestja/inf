ddd9b36ef27475c1c0ebc9ff883d5e2414ddccc2
Sergey Volodin <etoestja@yandex.ru>
2014-03-11 11:04:49 +0300

    DB 3

--- mipt/s4/DB/3/create.sql
+++ mipt/s4/DB/3/create.sql
@@ -27,7 +27,9 @@ go
 create table author
 (
 id int primary key identity(5000,1),
-[name] nvarchar(50) not null,
+name_first nvarchar(50) not null,
+surname nvarchar(50),
+name_middle nvarchar(50),
 birthDate datetime,
 deathDate datetime,
 constraint bdate check (birthDate <= deathDate)
@@ -55,10 +57,21 @@ go
 create table actor
 (
 id int primary key identity(5000,1),
-[name] nvarchar(50) not null,
+name_first nvarchar(50) not null,
+surname nvarchar(50),
+name_middle nvarchar(50),
 birthDate datetime,
-title int foreign key references actorTitle(id) on delete no action,
-gender int foreign key references gender(id) on delete no action
+title int foreign key references actorTitle(id) on delete no action on update cascade,
+gender int foreign key references gender(id) on delete no action on update cascade
+)
+
+go
+
+create table actor_actorTitle
+(
+id int primary key identity(5000,1),
+actor int foreign key references actor(id) on delete cascade on update no action,
+actorTitle int foreign key references actorTitle(id) on delete no action on update cascade
 )
 
 go
@@ -66,8 +79,8 @@ go
 create table actor_line
 (
 id int primary key identity(5000,1),
-actor_id int foreign key references actor(id) on delete cascade,
-line_id int foreign key references line(id) on delete cascade
+actor_id int foreign key references actor(id) on delete cascade on update cascade,
+line_id int foreign key references line(id) on delete cascade on update cascade
 )
 
 go
@@ -77,9 +90,9 @@ create table [role]
 id int primary key identity(5000,1),
 [name] nvarchar(50) not null,
 description nvarchar(500),
-gender int foreign key references gender(id) on delete no action,
-line int foreign key references line(id) on delete no action,
-[type] int foreign key references roleType(id) on delete no action
+gender int foreign key references gender(id) on delete no action on update cascade,
+line int foreign key references line(id) on delete no action on update cascade,
+[type] int foreign key references roleType(id) on delete no action on update cascade
 )
 
 go
@@ -87,7 +100,9 @@ go
 create table director
 (
 id int primary key identity(5000,1),
-[name] nvarchar(50) not null
+name_first nvarchar(50) not null,
+surname nvarchar(50),
+name_middle nvarchar(50)
 )
 
 go
@@ -111,8 +126,8 @@ create table seat
 id int primary key identity(5000,1),
 seat int not null,
 row int not null,
-room int foreign key references room(id) on delete no action,
-[type] int foreign key references seatType(id) on delete no action,
+room int foreign key references room(id) on delete no action on update cascade,
+[type] int foreign key references seatType(id) on delete no action on update cascade,
 constraint seatzero check (seat >= 0),
 constraint rowzero check (row >= 0)
 )
@@ -122,7 +137,9 @@ go
 create table staging
 (
 id int primary key identity(5000,1),
-director int foreign key references director(id) on delete no action
+[name] varchar(50) not null,
+description varchar(500),
+director int foreign key references director(id) on delete no action on update cascade
 )
 
 go
@@ -130,8 +147,8 @@ go
 create table role_staging
 (
 id int primary key identity(5000,1),
-[role] int foreign key references [role](id) on delete cascade,
-staging int foreign key references staging(id) on delete cascade
+[role] int foreign key references [role](id) on delete cascade on update cascade,
+staging int foreign key references staging(id) on delete cascade on update cascade
 )
 
 go
@@ -149,10 +166,10 @@ create table actor_rolestaging
 (
 id int primary key identity(5000,1),
 actor int foreign key references actor(id),
-role_staging int foreign key references role_staging(id) on delete cascade,
+role_staging int foreign key references role_staging(id) on delete cascade on update cascade,
 beginDate datetime not null,
 endDate datetime not null,
-[cast] int foreign key references [cast](id) on delete set null,
+[cast] int foreign key references [cast](id) on delete set null on update cascade,
 constraint dates check (beginDate <= endDate)
 )
 
@@ -199,8 +216,8 @@ go
 create table cast_staging
 (
 id int primary key identity(5000,1),
-[cast] int foreign key references [cast](id) on delete cascade,
-staging int foreign key references staging(id) on delete cascade,
+[cast] int foreign key references [cast](id) on delete cascade on update cascade,
+staging int foreign key references staging(id) on delete cascade on update cascade,
 )
 
 go
@@ -209,8 +226,8 @@ create table performance
 (
 id int primary key identity(5000,1),
 [date] datetime not null,
-staging int foreign key references staging(id) on delete no action,
-cast_staging int foreign key references cast_staging(id) on delete no action,
+staging int foreign key references staging(id) on delete no action on update cascade,
+cast_staging int foreign key references cast_staging(id) on delete no action on update no action,
 )
 
 go
@@ -218,8 +235,8 @@ go
 create table actorrolestaging_performance
 (
 id int primary key identity(5000,1),
-actorrolestaging int foreign key references actor_rolestaging(id) on delete cascade,
-performance int foreign key references performance(id) on delete cascade,
+actorrolestaging int foreign key references actor_rolestaging(id) on delete cascade on update no action,
+performance int foreign key references performance(id) on delete cascade on update no action,
 )
 
 go
@@ -228,7 +245,7 @@ create table seat_performance
 (
 id int primary key identity(5000,1),
 seat int foreign key references seat(id),
-performance int foreign key references performance(id) on delete cascade
+performance int foreign key references performance(id) on delete cascade on update cascade
 )
 
 go
--- mipt/s4/DB/3/insert.sql
+++ mipt/s4/DB/3/insert.sql
@@ -50,6 +50,7 @@ SET IDENTITY_INSERT [role] ON
 
 insert into [role](id, [name], gender, line, [type]) values (1,  N'техничка-шпионка', 2, 17, 4);
 insert into [role](id, [name], gender, line, [type]) values (2,  N'Гамлет', 1, 3, 1);
+insert into [role](id, [name], gender, line, [type]) values (3,  N'Горацио', 1, 3, 1);
 
 SET IDENTITY_INSERT [role] OFF
 
@@ -57,7 +58,13 @@ go
 
 SET IDENTITY_INSERT author ON
 
-insert into author(id, [name], birthDate, deathDate) values (1, N'Шекспир', 2013-01-01, 2014-01-01)
+insert into author(id, name_first, name_middle, surname, birthDate, deathDate) values (1, N'Михаил', N'Афанасьевич', N'Булгаков', 1891-05-03, 1940-03-10);
+insert into author(id, name_first, name_middle, surname, birthDate, deathDate) values (2, N'Уильям', NULL, N'Шекспир', 1564-04-26, 1616-04-23);
+insert into author(id, name_first, name_middle, surname, birthDate, deathDate) values (3, N'Николай', N'Васильевич', N'Гоголь', 1809-03-20, 1852-02-21);
+insert into author(id, name_first, name_middle, surname, birthDate, deathDate) values (4, N'Максим', NULL, N'Горький', 1868-03-16, 1936-06-18);
+insert into author(id, name_first, name_middle, surname, birthDate, deathDate) values (5, N'Антон', N'Павлович', N'Чехов', 1860-01-17, 1904-07-02);
+
+
 
 SET IDENTITY_INSERT author OFF
 
@@ -65,17 +72,88 @@ go
 
 SET IDENTITY_INSERT play ON
 
-insert into play(id, [name], description) values (1,  N'Плохая пьеса', N'дескрипшен')
+insert into play(id, [name], description) values (1,  N'Иван Васильевич', N'Создана в 1934—1936 годах на основе пьесы «Блаженство». При жизни автора не ставилась и не публиковалась (впервые опубликована в 1965 году). Послужила основой фильма «Иван Васильевич меняет профессию».')
+insert into play(id, [name], description) values (2,  N'Адам и Ева', N'«Адам и Ева» — пьеса Михаила Булгакова, в которой описан техногенный конец света в результате газовой войны. Пьесу не смогли опубликовать или поставить при жизни Булгакова и впервые она вышла в 1971 году, в Париже. В СССР её выпустили в октябре 1987 года.')
+insert into play(id, [name], description) values (3,  N'Враги', N'Пьеса «Враги» была представлена читателям в 1906 году в сборнике товарищества «Знание» и была признана первым произведением, написанным в жанре социалистического реализма. В этот период Горький активно сотрудничал с различными революционными организациями, что и послужило толчком для написания данного произведения.')
+insert into play(id, [name], description) values (4,  N'Егор Булычов и другие', N'Пьеса Максима Горького. Подзаголовок: Сцены.')
+insert into play(id, [name], description) values (5,  N'На дне', N'Пьеса Максима Горького, написанная в конце 1901 — начале 1902 года. Первоначальные названия — «Без солнца», «Ночлежка», «Дно», «На дне жизни»[1].')
+insert into play(id, [name], description) values (6,  N'Три сестры', N'Пьеса в четырёх действиях А. П. Чехова, написанная в 1900 году. Впервые была опубликована в журнале «Русская мысль», № 2 за 1901 год; с изменениями и поправками вышла отдельным изданием в том же году в издательстве А. Ф. Маркса[1].')
+insert into play(id, [name], description) values (7,  N'Гамлет', N'трагедия Уильяма Шекспира в пяти актах, одна ')
+
 
 SET IDENTITY_INSERT play OFF
 
 go
 
-insert into play_author(play, author) values (1, 1)
+insert into play_author(play, author) values (1, 1);
+insert into play_author(play, author) values (2, 1);
+insert into play_author(play, author) values (3, 4);
+insert into play_author(play, author) values (4, 4);
+insert into play_author(play, author) values (5, 4);
+insert into play_author(play, author) values (6, 5);
+insert into play_author(play, author) values (7, 2);
 
 go
 
-insert into role_play(role, play) values (1, 1)
+insert into role_play(role, play) values (2, 7)
+
+go
+
+SET IDENTITY_INSERT actorTitle ON
+
+insert into actorTitle(id, description) values (1, N'Заслуженный артист РСФСР');
+insert into actorTitle(id, description) values (2, N'Заслуженный артист Российской Федерации');
+insert into actorTitle(id, description) values (3, N'Заслуженный артист Императорских театров');
+insert into actorTitle(id, description) values (4, N'Народный артист СССР');
+insert into actorTitle(id, description) values (5, N'Народный артист РСФСР');
+insert into actorTitle(id, description) values (6, N'Народный артист Российской Федерации');
+
+SET IDENTITY_INSERT actorTitle OFF
+
+go
+
+SET IDENTITY_INSERT actor ON
+
+insert into actor(id, name_first, name_middle, surname, birthDate, gender) values (1, N'Джозеф', NULL, N'Файнс', 1970-05-27, 1);
+insert into actor(id, name_first, name_middle, surname, birthDate, gender) values (2, N'Александр', NULL, N'Дербенев', 1994-01-01, 1);
+
+SET IDENTITY_INSERT actor OFF
+
+go
+
+insert into actor_actorTitle(actor, actorTitle) values (2, 6);
+
+go
+
+SET IDENTITY_INSERT director ON
+
+insert into director(id, name_first, name_middle, surname) values(1, N'Стивен', NULL, N'Моффат');
+
+SET IDENTITY_INSERT director OFF
+
+go
+
+SET IDENTITY_INSERT staging ON
+insert into staging(id, director, [name], description) values (1, 1, 'Доктор Кто', NULL);
+insert into staging(id, director, [name], description) values (2, 1, 'На дне (новая версия)', N'Другой сюжет');
+insert into staging(id, director, [name], description) values (3, 1, 'Гамлет', NULL);
+SET IDENTITY_INSERT staging OFF
+
+go
+
+insert into play_staging(play, staging) values(7, 3);
+
+go
+
+SET IDENTITY_INSERT role_staging ON
+insert into role_staging(id, [role], staging) values (1, 2, 3);
+SET IDENTITY_INSERT role_staging OFF
+
+go
+
+SET IDENTITY_INSERT actor_rolestaging ON
+--insert into actor_rolestaging(id, [role], staging, 
+SET IDENTITY_INSERT actor_rolestaging OFF
 
 go
 
