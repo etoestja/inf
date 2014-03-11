4e85372cb1813b458745fd59b8370980098c1c40
Sergey Volodin <etoestja@yandex.ru>
2014-03-11 21:08:48 +0300

    DB 3

--- mipt/s4/DB/3/alter_example.sql
+++ mipt/s4/DB/3/alter_example.sql
@@ -26,14 +26,56 @@ drop constraint FK__castReplace__b__4C0144E4;
 
 go
 
+alter table cast_staging
+drop constraint FK__cast_stagi__cast__4FD1D5C8
+
+go
+
+alter table actor_rolestaging
+drop constraint FK__actor_role__cast__3BCADD1B
+
+go
+
+alter table [cast]
+drop constraint PK__cast__361203C5
+
+alter table [cast]
+alter column id int;
+
+alter table [cast]
+add constraint PK__cast__361203C5 primary key (id);
+
+go
+
 alter table castReplace
-add constraint FK__castReplace__a__4B0D20AB foreign key (a) references [cast](id);
+alter column a int
+
+alter table castReplace
+alter column b int
+
+alter table actor_rolestaging
+alter column [cast] int
+
+alter table cast_staging
+alter column [cast] int
+
+go
+
+alter table castReplace
+add constraint FK__castReplace__a__4B0D20AB foreign key (a) references [cast](id) on delete cascade;
 
 alter table castReplace
 add constraint FK__castReplace__b__4C0144E4 foreign key (b) references [cast](id)
 
-/*alter table [cast]
-alter column id bigint;*/
-/*
-alter table [cast]
-alter column id bigint;*/
+alter table castReplace
+add constraint neq check (not (a = b))
+
+go
+
+alter table cast_staging
+add constraint FK__cast_stagi__cast__4FD1D5C8 foreign key ([cast]) references [cast](id) on delete cascade on update cascade;
+
+go
+
+alter table actor_rolestaging
+add constraint FK__actor_role__cast__3BCADD1B foreign key ([cast]) references [cast](id) on delete cascade on update cascade;
