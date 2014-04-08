99f85db196cf641f3d217f56d072d4518cb97545
Sergey Volodin <etoestja@yandex.ru>
2014-04-08 12:31:09 +0400

    DB 6

---
+++ mipt/s4/DB/6/test/q3.sql
@@ -0,0 +1,29 @@
+﻿use theater;
+
+--possible
+
+set transaction isolation level repeatable read;
+
+--update actor set name_middle = N'Петров' where id = 2
+
+drop table actor1
+
+create table actor1(name_middle nvarchar(50));
+
+begin transaction
+
+insert into actor1(name_middle)
+select name_middle from actor where id = 2
+
+
+select * from actor1
+
+select * from actor
+where id = 2
+
+--update actor1 set name_middle = N'Петров'
+
+update actor set name_middle = (select top 1 name_middle from actor1)
+where id = 2
+
+commit;
---
+++ mipt/s4/DB/6/test/q4.sql
@@ -0,0 +1,11 @@
+﻿use theater;
+
+--possible
+
+set transaction isolation level repeatable read;
+
+begin transaction
+
+update actor set name_middle = N'Иванов' where id = 2
+
+commit
