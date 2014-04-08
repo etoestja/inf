2e5e336cff86fcee0ed035229799525b61835428
Sergey Volodin <etoestja@yandex.ru>
2014-04-08 12:48:51 +0400

    DB 6

--- mipt/s4/DB/6/1/1/q3.sql
+++ mipt/s4/DB/6/1/1/q3.sql
@@ -15,20 +15,12 @@ begin transaction
 insert into actor1(name_middle)
 select name_middle from actor where id = 2
 
-go
-
 select * from actor1
-
-go
 
 select * from actor
 where id = 2
 
---go
-
 --update actor1 set name_middle = N'Петров'
-
-go
 
 update actor set name_middle = (select top 1 name_middle from actor1)
 where id = 2
