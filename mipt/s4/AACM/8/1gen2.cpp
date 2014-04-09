e4e27b3f0cbf31c3f732048342527f787dc67530
Sergey Volodin <etoestja@yandex.ru>
2014-04-09 21:50:16 +0400

    AACM 8

--- mipt/s4/AACM/8/1gen2.cpp
+++ mipt/s4/AACM/8/1gen2.cpp
@@ -10,7 +10,7 @@ int main()
     int i;
     for(i = 0; i < N; i++)
     {
-        printf("%da_+-%da_-+%d+c_+-c_-+T+t_{%d}=0\\\\\n", x[i], x[i], y[i], 2 * i + 1);
-        printf("-%da_++%da_--%d-c_++c_-+T+t_{%d}=0\\\\\n\n", x[i], x[i], y[i], 2 * i + 2);
+        printf("t_{%d}=-%d-%da_++%da_--c_++c_--T\\\\\n", 2 * i + 1, y[i], x[i], x[i]);
+        printf("t_{%d}=%d+%da_+-%da_-+c_+-c_--T\\\\\n", 2 * i + 2, y[i], x[i], x[i]);
     }
 }
