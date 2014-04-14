520c29961f4006cc19b41c2be73c1b89ca61888d
Sergey Volodin <etoestja@yandex.ru>
2014-04-14 22:50:15 +0400

    DB 7

---
+++ mipt/s4/DB/7/q1.sql
@@ -0,0 +1,30 @@
+exec sp_revokedbaccess 'test'
+go
+
+exec sp_droplogin test
+
+go
+
+exec sp_addlogin @loginame = 'test', @passwd = 'czflamfqbm'
+
+go
+
+exec sp_grantdbaccess 'test'
+
+go
+
+use theater;
+grant select, insert, update, delete on actor to test;
+grant select (description), update (description) on gender to test;
+grant select on play to test;
+grant select, insert, update, delete on view1 to test;
+
+go
+exec sp_droprole role1
+go
+
+exec sp_addrole role1
+
+go
+
+grant update(play) on view2 to role1
---
+++ mipt/s4/DB/7/q2.sql
@@ -0,0 +1,31 @@
+use theater;
+
+select * from staging
+
+go
+
+select * from actor
+
+go
+
+select * from gender
+
+go
+
+select description from gender;
+
+go
+
+update play set description = 'f'
+
+go
+
+select * from play
+
+go
+
+select * from view1
+
+go
+
+update view3 set play = 'df'
