47f160b3f576727215796abf4defdd5f6bab1aa8
Sergey Volodin <etoestja@yandex.ru>
2014-05-05 19:19:54 +0400

    DB A

--- mipt/s4/DB/A/q2.sql
+++ mipt/s4/DB/A/q2.sql
@@ -1,6 +1,12 @@
 use NWC;
 
+create clustered index customerIDIndex on customers (customerID)
+create nonclustered index customerCity on customers (city)
+
+create clustered index customerIDIndex on orders (customerID)
+create nonclustered index shipCityInex on orders (shipCity)
+
 select * from customers, orders
 where customers.customerID = orders.customerID
 and customers.city = N'Berlin'
-and orders.ShipCity = N'Berlin'
+and orders.shipCity = N'Berlin'
--- mipt/s4/DB/A/q3.sql
+++ mipt/s4/DB/A/q3.sql
@@ -1,9 +1,21 @@
 use NWC;
 
+create clustered index orderID on orders (orderID)
+create nonclustered index customerID on orders (customerID)
+create nonclustered index employeeID on orders (employeeID)
+create nonclustered index shipVia on orders (shipVia)
+
+create clustered index orderID on orderDetails (orderID)
+create nonclustered index productID on orderDetails (productID)
+
+create clustered index productID on products (productID)
+
+create clustered index customerID on customers (customerID)
+
 select * from orders, products, customers, orderDetails
 where orders.customerID = customers.customerID
 and orders.orderID = orderDetails.orderID
 and orderDetails.productID = products.productID
-and substring(customers.customerID, 1, 1) = N'R'
+and customers.customerID > N'N'
 and employeeID = 9
 and shipVia = 2
