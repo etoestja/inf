41acfec7da80a6171af47f2a2b9b4b8bcf35aee0
Sergey Volodin <etoestja@yandex.ru>
2014-05-06 09:39:05 +0400

    DB 9

--- mipt/s4/DB/9/ex.sql
+++ mipt/s4/DB/9/ex.sql
@@ -15,7 +15,7 @@ go
 
 go
 
-create table t1 (id int primary key identity(1,1), data datetime default CURRENT_TIMESTAMP)
+create table t1 (id int primary key identity(1,1), data int)
 
 go
 
@@ -28,28 +28,12 @@ go
 go
 
 create trigger tr1 on t1
-for delete
+for insert
 as
 
 select * from inserted
-
-delete from t1
---insert into t1 values(CURRENT_TIMESTAMP)
-
---SET IDENTITY_INSERT t1 ON
---insert into t1(id, data) values(2, CURRENT_TIMESTAMP)
---insert into t1(data) values(CURRENT_TIMESTAMP)
---SET IDENTITY_INSERT t1 OFF
-
-select * from inserted
+select * from deleted
 
 go
 
-insert into t1 values(NULL)
-insert into t1 values(NULL)
-
-delete from t1
-
-go
-
-use master;
+insert into t1 values(1)
