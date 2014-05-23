b3d0c9512a99beaa9aa2cb6f73a3e3c8efa663c5
Sergey Volodin <etoestja@yandex.ru>
2014-05-24 02:49:14 +0400

    AACM D 48.0

--- mipt/s4/AACM/D/dfs.cpp
+++ mipt/s4/AACM/D/dfs.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+
+using std::vector;
 
 #define NMAX 100
 
@@ -10,22 +13,65 @@ int arr[NMAX][NMAX];
 int d[NMAX], f[NMAX];
 bool visited[NMAX];
 
+int type[NMAX][NMAX];
+
+enum _type{EDGE_TREE = 0, EDGE_BACK, EDGE_FORWARD, EDGE_CROSS};
+const char* _typeN[] = {"tree", "back", "forward", "cross"};
+
 int N;
 
 int timeC;
 
-void dfs(int v)
+bool contains(vector<int> &a, int b)
 {
+    for(vector<int>::iterator it = a.begin(); it != a.end(); it++)
+        if(*it == b)
+            return(true);
+    return(false);
+}
+
+void dfs(int v, vector<int> prevVisited)
+{
+    cout << "dfs from " << v << endl;
     if(visited[v]) return;
 
     visited[v] = true;
     d[v] = timeC++;
 
+    int K = prevVisited.size();
+    for(int i = 0; i < K - 1; i++)
+    {
+        if(arr[prevVisited[i]][v]) type[prevVisited[i]][v] = EDGE_FORWARD;
+    }
+
+    vector<int> prevVisitedV = prevVisited;
+    prevVisitedV.push_back(v);
+
     for(int i = 0; i < N; i++)
-        if(arr[v][i] && i != v && !visited[i])
-            dfs(i);
+        if(arr[v][i] && i != v)
+        {
+            if(!visited[i])
+            {
+                type[v][i] = EDGE_TREE;
+                dfs(i, prevVisitedV);
+            }
+            else if(contains(prevVisited, i))
+                type[v][i] = EDGE_BACK;
+        }
 
     f[v] = timeC++;
+}
+
+void prepare()
+{
+    timeC = 1;
+
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            type[i][j] = EDGE_CROSS;
+
+    for(int i = 0; i < N; i++)
+        visited[i] = false;
 }
 
 void dump()
@@ -46,11 +92,31 @@ void dump()
     cout << endl;
 }
 
+void printEdges()
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+            if(arr[i][j])
+            {
+                cout << "(" << (i + 1) << "," << (j + 1) << ")\t" << _typeN[type[i][j]] << " ";
+                if(d[i] < d[j] && d[j] < f[j] && f[j] < f[i])
+                    cout << "T or F";
+                if(d[j] <= d[i] && d[i] < f[i] && f[i] <= f[j])
+                    cout << "B";
+                if(d[j] < f[j] && f[j] < d[i] && d[i] < f[i])
+                    cout << "C";
+                cout << endl;
+            }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    timeC = 1;
-
     cin >> N;
+
+    prepare();
 
     for(int i = 0; i < N; i++)
         for(int j = 0; j < N; j++)
@@ -58,10 +124,12 @@ int main()
 
     for(int i = 0; i < N; i++)
     {
-        dfs(i);
+        dfs(i, vector<int>());
         cout << "dfs from " << i << " finished" << endl;
         dump();
     }
+
+    printEdges();
 
     return(0);
 }
