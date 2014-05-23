61eafc83f8186e1c5cf199a2bb48d565844075d6
Sergey Volodin <etoestja@yandex.ru>
2014-05-24 02:00:50 +0400

    AACM D 48.0

--- mipt/s4/AACM/D/dfs.cpp
+++ mipt/s4/AACM/D/dfs.cpp
@@ -28,6 +28,24 @@ void dfs(int v)
     f[v] = timeC++;
 }
 
+void dump()
+{
+    cout << "==== DUMP (d, f) ====" << endl;
+    for(int i = 0; i < N; i++)
+    {
+        cout << d[i] << "\t";
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < N; i++)
+    {
+        cout << f[i] << "\t";
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     timeC = 1;
@@ -39,21 +57,11 @@ int main()
             cin >> arr[i][j];
 
     for(int i = 0; i < N; i++)
+    {
         dfs(i);
-
-    cout << "= d[.] =" << endl;
-    for(int i = 0; i < N; i++)
-    {
-        cout << d[i] << "\t";
+        cout << "dfs from " << i << " finished" << endl;
+        dump();
     }
-
-    cout << endl << "= f[.] =" << endl;
-    for(int i = 0; i < N; i++)
-    {
-        cout << f[i] << "\t";
-    }
-
-    cout << endl;
 
     return(0);
 }
