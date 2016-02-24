29536bb999a15a30d6526a1ae0f3316d8e038921
Sergey Volodin <etoestja@yandex.ru>
2016-02-24 21:12:58 +0300

    Add MPI t1 task & add comments

--- mipt/s6/mpi/t1/function.h
+++ mipt/s6/mpi/t1/function.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+// target function to integrate
 double f(double x);
 
 #endif
--- mipt/s6/mpi/t1/integrate.h
+++ mipt/s6/mpi/t1/integrate.h
@@ -1,10 +1,13 @@
 #ifndef INTEGRATE_H
 #define INTEGRATE_H
 
+// pointer function to integrate type (Double-to-Double Function)
 typedef double(*DDFunc)(double);
 
+// integrate with such N that eps(N) <= eps
 double integrateAuto(DDFunc f, double a, double b, double eps);
 
+// integrate with fixed N
 double integrate(DDFunc f, unsigned N, double a, double b);
 
 #endif
