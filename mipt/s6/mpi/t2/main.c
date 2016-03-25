20e8fc2a53087390e3c46b0109e08f2a859321ce
Sergey Volodin <etoestja@yandex.ru>
2016-03-26 00:20:16 +0300

    Add solution mpi t2

--- mipt/s6/mpi/t2/main.c
+++ mipt/s6/mpi/t2/main.c
@@ -4,6 +4,31 @@
 #include "exact.h"
 #include "config.h"
 
+#define NOCOUT
+
+int N, S;
+double tau;
+double h;
+
+inline void applyPattern(double* arrOld, double* arrNew, int index)
+{
+    double K = k * tau / h / h;
+    arrNew[index] = arrOld[index] + K * (arrOld[index - 1] - 2 * arrOld[index] + arrOld[index + 1]);
+}
+
+inline void swap(double** a, double** b)
+{
+    double* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void printPoint(int i, double val)
+{
+//    printf("T=%0.2f x=%0.2f i=%d u=%0.2f\n", T, i * h, i, val);
+    printf("%0.2f\t", val);
+}
+
 void processExchange(int rank, int size, double* arr, int n)
 {
     if(rank % 2 == 0)
@@ -40,49 +65,143 @@ void processExchange(int rank, int size, double* arr, int n)
     }
 }
 
-int N;
-double tau;
-double h;
-
 int main(int argc, char** argv)
 {
-    N = 10;
-    tau = 0.001;
+    double runBegin, runEnd;
+    T = 0.1;
+    k = 1;
+    u_0 = 1;
+    N = 100000;
+    S = 1000;
+    tau = T / S;
     h = l / N;
+
     // MPI process parameters
     int rank, size;
 
-/*    // MPI initialization
+    // MPI initialization
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // print my status
-    fprintf(stderr, "SIZE=%d RANK=%d\n", size, rank);
-*/
 
-    printDifferentialSingle();
-    printExact();
+    if(rank == 0)
+    {
+#ifndef NOCOUT
+        printExactEnd();
+#endif
+
+        runBegin = MPI_Wtime();
+    }
+
+//    printDifferentialSingle();
+//    printExact();
+
+    int p = size;
+    int M = N - 1;
+    int n = 1 + (M / p);
+    int n0 = M % p;
+    if(rank + 1 <= n0)
+        n++;
+
+#ifndef NOCOUT
+    fprintf(stderr, "SIZE=%d RANK=%d M=%d n=%d n0=%d\n", size, rank, M, n, n0);
+#endif
+
+    // this processor has n-1 active points
+    // 1...n-1
+    // also 0, n are shadow
+    // 1, n-1 will be sent
+    // 0, n will be received
+
+    double* arrOld = malloc(sizeof(double) * (n + 1));
+    double* arrNew = malloc(sizeof(double) * (n + 1));
+
+    arrOld[0] = 0;
+    arrNew[0] = 0;
+    arrOld[n] = 0;
+    arrNew[n] = 0;
+
+    int i, j;
+    // i for length
+    // j for time
+
+    for(i = 1; i <= n - 1; i++)
+        arrOld[i] = u_0;
+
+    for(j = 1; j <= S; j++)
+    {
+        for(i = 1; i <= n - 1; i++)
+            applyPattern(arrOld, arrNew, i);
+
+        // send 1, n-1
+        // recv 0, n
+
+        swap(&arrNew, &arrOld);
+
+        processExchange(rank, size, arrOld, n);
+    }
+
+//    for(i = 1; i <= n - 1; i++)
+//        printf("t=%0.2f rank=%d i=%d val=%0.2f\n", T, rank, i, arrOld[i]);
+
+#ifndef NOCOUT
+    if(rank == 0)
+    {
+        int k, i1;
+        MPI_Status status;
+        i = 0;
+
+        printf("DIFFERENTIAL:\n");
+        printPoint(0, 0);
+        i++;
+
+        for(i1 = 1; i1 <= n - 1; i1++)
+        {
+            printPoint(i, arrOld[i]);
+            i++;
+        }
+
+        for(k = 1; k < size; k++)
+        {
+            int i1 = 0;
+            int n1 = 0;
+            MPI_Recv(arrNew, sizeof(double) * (n - 1), MPI_BYTE, k, 2, MPI_COMM_WORLD, &status);
+            MPI_Get_count(&status, MPI_BYTE, &n1);
+            n1 /= sizeof(double);
+
+            for(i1 = 0; i1 < n1; i1++)
+            {
+                printPoint(i, arrNew[i1]);
+                i++;
+            }
+        }
+
+        printPoint(i, 0);
+        printf("\n");
+    }
+    else
+    {
+        MPI_Send(arrOld + 1, sizeof(double) * (n - 1), MPI_BYTE, 0, 2, MPI_COMM_WORLD);
+    }
+#endif
+
+    if(rank == 0)
+    {
+        runEnd = MPI_Wtime();
+        printf("WTIME=%lf\n", runEnd - runBegin);
+    }
+
+    MPI_Finalize();
 
     return(0);
 }
 
-inline void applyPattern(double* arrOld, double* arrNew, int index, double K)
-{
-    arrNew[index] = arrOld[index] + K * (arrOld[index - 1] - 2 * arrOld[index] + arrOld[index + 1]);
-}
-
-inline void swap(double** a, double** b)
-{
-    double* t = *a;
-    *a = *b;
-    *b = t;
-}
-
 int printDifferentialSingle()
 {
-    double* arrOld = malloc(sizeof(int) * (N + 1));
-    double* arrNew = malloc(sizeof(int) * (N + 1));
+    double* arrOld = malloc(sizeof(double) * (N + 1));
+    double* arrNew = malloc(sizeof(double) * (N + 1));
     int i, j;
     for(i = 0; i <= N; i++)
         arrOld[i] = u_0;
@@ -95,7 +214,7 @@ int printDifferentialSingle()
     for(i = 0; i < S; i++)
     {
         for(j = 1; j <= N - 1; j++)
-            applyPattern(arrOld, arrNew, j, k * tau / h / h);
+            applyPattern(arrOld, arrNew, j);
 
         arrNew[0] = 0;
         arrNew[N] = 0;
@@ -108,6 +227,16 @@ int printDifferentialSingle()
         swap(&arrNew, &arrOld);
     }
 
+    return(0);
+}
+
+int printExactEnd()
+{
+    double t, x;
+    printf("EXACT:\n");
+    for(x = 0; x <= l; x += h)
+        printf("%0.2f\t", getExactSolution(x, T));
+    printf("\n");
     return(0);
 }
 
