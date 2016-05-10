1eac9ee7ba592820c67379bebfc4fc8f1dd1d8b5
Sergey Volodin <etoestja@yandex.ru>
2016-05-10 21:35:56 +0400

    Add mes script

---
+++ mipt/s6/mpi/t4/measure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pmin=1
+pmax=15
+
+kn=5
+
+p=$pmin
+echo "["
+while (( $p <= $pmax ))
+do
+    k=0
+    echo -n "[$p, "
+    while (( $k < $kn ))
+    do
+        res1=$(./main $p 2>&1|grep TIME|grep -aEo "[0-9.]+")
+        echo -n "$res1, "
+        let k=k+1
+    done
+    echo "],"
+    let p=p+1
+done
+echo "]"
