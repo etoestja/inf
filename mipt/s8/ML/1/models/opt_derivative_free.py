4777430875019681452a9b932285c4d0c1c0c174
Sergei Volodin <etoestja@yandex.ru>
2017-03-29 12:30:38 +0300

    Add basic random search

--- mipt/s8/ML/1/models/opt_derivative_free.py
+++ mipt/s8/ML/1/models/opt_derivative_free.py
@@ -9,12 +9,19 @@ from multiprocessing import Pool
 
 def mult_random_search(f, params_mean, params_std=1., n_workers=2, batch_size=100, n_iter=50):
     """ Multiprocessing version of Random Search algorithm."""
-    params_std = np.ones_like(params_mean) * params_std
+#    params_std = np.ones_like(params_mean) * params_std
     best_params = params_mean
+    best_accuracy = 0
+    pool = Pool(processes=n_workers)
     for _ in range(n_iter):
-        pool = Pool(processes=n_workers)
+        params = params_std ** 0.5 * np.random.randn(*(params_mean.shape + (n_workers,))) + np.matlib.repmat(params_mean, n_workers, 1).T;
         # TO BE IMPLEMENTED: random search for parameters
-        yield {'results' : ys, 'best_params' : best_params}
+        params_arr = map(lambda x : x[:, 0], np.split(params, n_workers, axis = -1))
+        ys = np.array(pool.map(f, params_arr))
+        if np.max(ys) > best_accuracy:
+            best_params = params_arr[np.argmax(ys)]
+            best_accuracy = np.max(ys)
+        yield {'results' : ys, 'best_params' : best_params, 'best_accuracy' : best_accuracy}
 
 def mult_cem(f, params_mean, params_std=1., n_workers=2, batch_size=100, n_iter=50, elite_frac=0.2):
     """ Multiprocessing version of CEM algorithm."""
