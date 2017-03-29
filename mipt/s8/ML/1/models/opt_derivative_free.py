f7aa19d1c402c72f0c3d7a597d8512fc8ca9a95a
Sergei Volodin <etoestja@yandex.ru>
2017-03-29 13:35:35 +0300

    Add CEM method

--- mipt/s8/ML/1/models/opt_derivative_free.py
+++ mipt/s8/ML/1/models/opt_derivative_free.py
@@ -9,25 +9,33 @@ from multiprocessing import Pool
 
 def mult_random_search(f, params_mean, params_std=1., n_workers=2, batch_size=100, n_iter=50):
     """ Multiprocessing version of Random Search algorithm."""
-#    params_std = np.ones_like(params_mean) * params_std
     best_params = params_mean
     best_accuracy = 0
     pool = Pool(processes=n_workers)
     for _ in range(n_iter):
-        params = params_std ** 0.5 * np.random.randn(*(params_mean.shape + (batch_size,))) + np.matlib.repmat(best_params, batch_size, 1).T;
+        params = params_std * np.random.randn(*(params_mean.shape + (batch_size,))) + np.matlib.repmat(best_params, batch_size, 1).T;
         params_arr = map(lambda x : x[:, 0], np.split(params, batch_size, axis = -1))
         ys = np.array(pool.map(f, params_arr))
         if np.max(ys) > best_accuracy:
             best_params = params_arr[np.argmax(ys)]
             best_accuracy = np.max(ys)
         yield {'results' : ys, 'best_params' : best_params, 'best_accuracy' : best_accuracy}
+    pool.close()
 
 def mult_cem(f, params_mean, params_std=1., n_workers=2, batch_size=100, n_iter=50, elite_frac=0.2):
     """ Multiprocessing version of CEM algorithm."""
     n_elite = int(np.round(batch_size * elite_frac))
     params_std = np.ones_like(params_mean) * params_std
+    pool = Pool(processes=n_workers)
     for _ in range(n_iter):
-        pool = Pool(processes=n_workers)
-        # TO BE IMPLEMENTED: CEM for search of parameters
+        params = np.multiply(np.matlib.repmat(params_std, batch_size, 1).T, np.random.randn(*(params_mean.shape + (batch_size,)))) + np.matlib.repmat(params_mean, batch_size, 1).T;
+        params_arr = map(lambda x : x[:, 0], np.split(params, batch_size, axis = -1))
+        ys = np.array(pool.map(f, params_arr))
+        elite_idx = np.argsort(ys)[-n_elite:]
+        params_elite = params[:,elite_idx];
+        params_std = np.std(params_elite, axis = 1)
+        params_mean = np.mean(params_elite, axis = 1);
+        #print("New mean = %f std = %f" % (np.mean(params_mean), np.mean(params_std)))
+        best_params = params_arr[elite_idx[-1]]
         yield {'results' : ys, 'best_params' : best_params}
-
+    pool.close()
